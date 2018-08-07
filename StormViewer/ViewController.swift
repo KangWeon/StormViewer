dbe6060fc32e1fd2249c439127f9dc68deb1735e
KangWeon <kangweon.jeong@gmail.com>
2018-08-07 20:29:51 +0900

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by klarheit on 07/08/2018.
+//  Copyright © 2018 klarheit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
